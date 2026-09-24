3e26417ef1755fb7d64ece8a674bb9908a74745d
ohahbuji1988 <ohahbuji1988@gmail.com>
2026-09-25 02:01:00 +0900

    fix: resolve batch file encoding and parenthesis parser syntax error

--- run_app_window.bat
+++ run_app_window.bat
@@ -1,18 +1,17 @@
 @echo off
-chcp 65001 > nul
-echo [My 약] 모바일 앱 전용 창 모드로 실행 중입니다...
+title My Yak App Launcher
 
-:: Chrome이 설치되어 있는 경우
-if exist "C:\Program Files\Google\Chrome\Application\chrome.exe" (
-    start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --app=http://localhost:8080 --window-size=420,915
-    exit /b
-)
+if exist "C:\Program Files\Google\Chrome\Application\chrome.exe" goto RUN_CHROME
+if exist "C:\Program Files\Microsoft\Edge\Application\msedge.exe" goto RUN_EDGE
+if exist "%ProgramFiles(x86)%\Microsoft\Edge\Application\msedge.exe" goto RUN_EDGE
 
-:: Edge가 설치되어 있는 경우
-if exist "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" (
-    start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --app=http://localhost:8080 --window-size=420,915
-    exit /b
-)
-
-:: 기본 브라우저로 열기
 start http://localhost:8080
+exit /b
+
+:RUN_CHROME
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --app=http://localhost:8080 --window-size=420,915
+exit /b
+
+:RUN_EDGE
+start "" msedge.exe --app=http://localhost:8080 --window-size=420,915
+exit /b
